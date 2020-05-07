6430830a205fa2b74d09690494614a2f5cff133b
SetraR <setrar@valiha.com>
2020-05-06 21:54:57 -0400

    .

--- src/test/scala/africa/gasy/fanorona/generator/MoveSpec.scala
+++ src/test/scala/africa/gasy/fanorona/generator/MoveSpec.scala
@@ -12,16 +12,36 @@ class MoveSpec extends FlatSpec {
 
     // scalastyle:off
 
-    val myPieces = 544790103982080L
-    val opponentPieces = 562399469895680L
+    val myPieces = BigInt(List("0000"
+      , "000000000"
+      , "000000000"
+      , "101001010"
+      , "111101111"
+      , "111101111"
+      ).mkString, 2).toLong // 544790103982080L
+    val opponentPieces = BigInt(List("0000"
+      , "000000000"
+      , "000000000"
+      , "101001010"
+      , "111111111"
+      , "111111111"
+    ).mkString, 2).toLong // 562399469895680L
+
+    val storedTo = Bits.ON_BOARD & ~(myPieces | opponentPieces)
+
     val nextSet: Long =
-      BigInt("0000000000000000000000000000000000001010010101010010100000000000",
-             2).toLong
+      BigInt(List("0000"
+          , "000000000"
+          , "101001010"
+          , "010100101"
+          , "000000000"
+          , "000000000"
+        ).mkString, 2).toLong
 
     assert(
       Move.findNextSet(ingressMoveSetIndex = -1,
                        storedFrom = myPieces,
-                       storedTo = Bits.ON_BOARD & ~(myPieces | opponentPieces),
+                       storedTo = storedTo,
                        opponentPieces = opponentPieces) ===
         ElementSet(1,
                    shift = Bits.SHIFT_VERTICAL,
